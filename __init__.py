f235be6ae12174f144d438353c954ad287535b1e
Jiri Kuncar <jiri.kuncar@cern.ch>
2013-09-27 15:01:56 +0200

    global: new code structure
    * Adds Sphinx documentation.
    
    * Moves Jinja2 templates to the new code structure
      `invenio/modules/<name>/templates/<name>/index.html`.
    
    * Adds new tag module developed by Krzysztof Lis
      <krzysztof.lis@cern.ch>.
    
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Co-authored-by: Esteban J. G. Gabancho <esteban.gabancho@cern.ch>
    Co-authored-by: Krzysztof Lis <krzysztof.lis@cern.ch>
    Co-authored-by: Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
    
    NOTE: beware, patch amended by Tibor to use new file names

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#
+##
 ## This file is part of Invenio.
 ## Copyright (C) 2013 CERN.
 ##
@@ -15,26 +15,4 @@
 ##
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
-## 59 Temple Place, Suite 330, Boston, MA 02D111-1307, USA.
-
-"""
-GoTo database models.
-"""
-
-# General imports.
-from invenio.ext.sqlalchemy import db
-
-# Create your models here.
-
-
-class Goto(db.Model):
-    """Represents a Goto record."""
-    __tablename__ = 'goto'
-    label = db.Column(db.String(150), primary_key=True)
-    plugin = db.Column(db.String(150), nullable=False)
-    parameters = db.Column(db.Text, nullable=False)
-    creation_date = db.Column(db.DateTime, nullable=False, index=True)
-    modification_date = db.Column(db.DateTime, nullable=False, index=True)
-
-
-__all__ = ['Goto']
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
